74a49ab2c96f03eff7af57a2d7230110f778b1cc
asandherqa <asandherqa@gmail.com>
2021-07-15 21:08:24 +0000

    ic

--- class_api/app.py
+++ class_api/app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 def get_class():
 
     firstnameclass = requests.post('http://pryze_server:5000/firstname')
+    return firstnameclass
 
-    class1 = ["Ferrari", "Lamborghini", "Aston Martin", "McLaren", "Bentley", "Porche"]
-    class2 = ["Candle", "Toaster", "Golf Clubs", "Speaker", "Headphones", "Book"]
+    # class1 = ["Ferrari", "Lamborghini", "Aston Martin", "McLaren", "Bentley", "Porche"]
+    # class2 = ["Candle", "Toaster", "Golf Clubs", "Speaker", "Headphones", "Book"]
 
-    if 1 < len(firstnameclass) <= 5:
-        return class1[request.data.decode('utf-8')]
-    else:
-        return class2[request.data.decode('utf-8')]
+    # if 1 < len(firstnameclass) <= 5:
+    #     return class1[request.data.decode('utf-8')]
+    # else:
+    #     return class2[request.data.decode('utf-8')]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
